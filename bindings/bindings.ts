83e6f02d99a2fb3e713bba5fd46b8cd3229eddf1
Divy Srivastava <dj.srivastava23@gmail.com>
2021-12-28 22:16:10 +0530

    upgrade deno_bindgen to 0.4.1

--- bindings/bindings.ts
+++ bindings/bindings.ts
@@ -1,20 +1,15 @@
 // Auto-generated with deno_bindgen
-import { CachePolicy, prepare } from "https://deno.land/x/plug@0.4.1/plug.ts";
+import { CachePolicy, prepare } from "https://deno.land/x/plug@0.4.1/plug.ts"
 function encode(v: string | Uint8Array): Uint8Array {
-  if (typeof v !== "string") return v;
-  return new TextEncoder().encode(v);
+  if (typeof v !== "string") return v
+  return new TextEncoder().encode(v)
 }
 const opts = {
   name: "deno_sqlite3",
   url: (new URL("../target/release", import.meta.url)).toString(),
   policy: undefined,
-};
+}
 const _lib = await prepare(opts, {
-  sqlite3_execute: {
-    parameters: ["usize", "pointer", "usize", "pointer", "usize"],
-    result: "isize",
-    nonblocking: true,
-  },
   fill_result: {
     parameters: ["pointer", "usize"],
     result: "void",
@@ -25,8 +20,19 @@ const _lib = await prepare(opts, {
     result: "void",
     nonblocking: false,
   },
+  get_result_len: { parameters: [], result: "usize", nonblocking: false },
+  sqlite3_execute: {
+    parameters: ["usize", "pointer", "usize", "pointer", "usize"],
+    result: "isize",
+    nonblocking: true,
+  },
   deno_sqlite3_open: {
     parameters: ["usize", "pointer", "usize"],
+    result: "isize",
+    nonblocking: true,
+  },
+  deno_sqlite3_close: {
+    parameters: ["usize"],
     result: "isize",
     nonblocking: true,
   },
@@ -43,45 +49,45 @@ const _lib = await prepare(opts, {
     result: "isize",
     nonblocking: true,
   },
-  get_result_len: { parameters: [], result: "usize", nonblocking: false },
   sqlite3_open_memory: {
     parameters: ["usize"],
     result: "isize",
     nonblocking: true,
   },
-  deno_sqlite3_close: {
-    parameters: ["usize"],
-    result: "isize",
-    nonblocking: true,
-  },
-});
+})
 
+export function fill_result(a0: Uint8Array) {
+  const a0_buf = encode(a0)
+  return _lib.symbols.fill_result(a0_buf, a0_buf.byteLength) as null
+}
+export function get_last_error(a0: Uint8Array) {
+  const a0_buf = encode(a0)
+  return _lib.symbols.get_last_error(a0_buf, a0_buf.byteLength) as null
+}
+export function get_result_len() {
+  return _lib.symbols.get_result_len() as number
+}
 export function sqlite3_execute(a0: number, a1: string, a2: Uint8Array) {
-  const a1_buf = encode(a1);
-  const a2_buf = encode(a2);
+  const a1_buf = encode(a1)
+  const a2_buf = encode(a2)
   return _lib.symbols.sqlite3_execute(
     a0,
     a1_buf,
     a1_buf.byteLength,
     a2_buf,
     a2_buf.byteLength,
-  ) as Promise<number>;
-}
-export function fill_result(a0: Uint8Array) {
-  const a0_buf = encode(a0);
-  return _lib.symbols.fill_result(a0_buf, a0_buf.byteLength) as null;
-}
-export function get_last_error(a0: Uint8Array) {
-  const a0_buf = encode(a0);
-  return _lib.symbols.get_last_error(a0_buf, a0_buf.byteLength) as null;
+  ) as Promise<number>
 }
 export function deno_sqlite3_open(a0: number, a1: string) {
-  const a1_buf = encode(a1);
+  const a1_buf = encode(a1)
   return _lib.symbols.deno_sqlite3_open(
     a0,
     a1_buf,
     a1_buf.byteLength,
-  ) as Promise<number>;
+  ) as Promise<number>
+}
+export function deno_sqlite3_close(a0: number) {
+  return _lib.symbols.deno_sqlite3_close(a0) as Promise<number>
 }
 export function sqlite3_query(
   a0: number,
@@ -89,9 +95,9 @@ export function sqlite3_query(
   a2: Uint8Array,
   a3: Uint8Array,
 ) {
-  const a1_buf = encode(a1);
-  const a2_buf = encode(a2);
-  const a3_buf = encode(a3);
+  const a1_buf = encode(a1)
+  const a2_buf = encode(a2)
+  const a3_buf = encode(a3)
   return _lib.symbols.sqlite3_query(
     a0,
     a1_buf,
@@ -100,14 +106,8 @@ export function sqlite3_query(
     a2_buf.byteLength,
     a3_buf,
     a3_buf.byteLength,
-  ) as Promise<number>;
-}
-export function get_result_len() {
-  return _lib.symbols.get_result_len() as number;
+  ) as Promise<number>
 }
 export function sqlite3_open_memory(a0: number) {
-  return _lib.symbols.sqlite3_open_memory(a0) as Promise<number>;
-}
-export function deno_sqlite3_close(a0: number) {
-  return _lib.symbols.deno_sqlite3_close(a0) as Promise<number>;
+  return _lib.symbols.sqlite3_open_memory(a0) as Promise<number>
 }
