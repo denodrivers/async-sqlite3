3bedd64d66ad4400a95e7051361aa37637d6eef0
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-12-31 19:57:53 -0500

    :recycle: Update syntax

--- bench/python-sqlite3/main.py
+++ bench/python-sqlite3/main.py
@@ -1,34 +1,40 @@
-import time
 import sqlite3
+import time
+
 import rapidjson
+
 
 def perform_jobs(conn, jobs):
     cursor = conn.cursor()
+
     for job in jobs:
-        cursor.execute(job['text'], tuple(job['params']) if 'params' in job else ())
+        cursor.execute(job["text"], (job["params"]) if "params" in job else ())
         conn.commit()
 
-def perform_workflow(workflow):
 
-    conn = sqlite3.connect(workflow['specifier'])
-    
-    perform_jobs(conn, workflow['setupJobs'])
+def perform_workflow(workflow):
+    conn = sqlite3.connect(workflow["specifier"])
+
+    perform_jobs(conn, workflow["setupJobs"])
 
     start = time.time() * 1000
-    
-    for _ in range(workflow['iterations']):
-      perform_jobs(conn, workflow['jobs'])
+
+    for _ in range(workflow["iterations"]):
+        perform_jobs(conn, workflow["jobs"])
+
     conn.commit()
     conn.close()
+
     return time.time() * 1000 - start
 
-file = open("../workflows.json")
-json = file.read()
-workflows = rapidjson.loads(json)
+
+def get_workflows():
+    with open("../workflows.json") as file:
+        return rapidjson.loads(file.read())
+
 
 print("python-sqlite3")
 
-for workflow in workflows:
+for workflow in get_workflows():
     t = perform_workflow(workflow)
-    print("  {:s}: {:.4f}ms".format(workflow['name'], t))
-
+    print(f"  {workflow['name']:s}: {t:.4f}ms")
